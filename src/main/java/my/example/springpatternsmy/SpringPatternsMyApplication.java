275f8a1bc83269f090dab0aa5a08792b8a8e3427
IlyaRum <tekstir@yandex.ru>
2025-01-24 13:34:58 +0300

    init

---
+++ src/main/java/my/example/springpatternsmy/SpringPatternsMyApplication.java
@@ -0,0 +1,13 @@
+package my.example.springpatternsmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPatternsMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPatternsMyApplication.class, args);
+    }
+
+}
